c0c75bae24d4ffec3623f4dbeac016f530a31996
Avesh Kumar <avesh.k@purplle.com>
2025-07-31 11:44:25 +0530

    tailwind

--- solid-demo-refine/src/pages/categories/CategoriesPage.tsx
+++ solid-demo-refine/src/pages/categories/CategoriesPage.tsx
@@ -123,6 +123,9 @@ const CategoriesPage: React.FC = () => {
           </div>
         )}
       </div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       
       <div className="categories-container">
         {categories.map((category) => (
